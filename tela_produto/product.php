df5f850552d7c6027cc3096b2bfa90eeb5bd8044
deiseeuf <165103420+deiseeuf@users.noreply.github.com>
2025-06-09 12:04:41 -0300

    Update product.php

---
+++ tela_produto/product.php
@@ -0,0 +1,18 @@
+<?php include 'get_product.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $produto['nome']; ?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="product-container">
+    <h1><?php echo $produto['nome']; ?></h1>
+    <img src="<?php echo $produto['imagem']; ?>" alt="Imagem do produto">
+    <p class="preco">R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
+    <p><?php echo $produto['descricao']; ?></p>
+    <button onclick="alert('Produto adicionado ao carrinho!')">Adicionar ao Carrinho</button>
+  </div>
+</body>
+</html>
